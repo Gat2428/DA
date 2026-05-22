2570d625453658dc53e9968838c280af2c46cb59
Gat2428 <albuscharlie44@gmail.com>
2026-05-22 09:04:55 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,51 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 int count;
-void merge(int a[10], int left, int mid, int right)
-{
- int i, j, k, b[10];
- i = left;
- j = mid + 1;
- k = left;
- while ((i <= mid) && (j <= right))
- {
- count++;
- if (a[i] < a[j])
- b[k++] = a[i++];
- else
- b[k++] = a[j++];
- }
- while (i <= mid)
- b[k++] = a[i++];
- while (j <= right)
- b[k++] = a[j++];
- for (i = left; i <= right; i++)
- a[i] = b[i];
+int partition(int a[10], int low, int high) {
+int i,j,pivot,temp;
+i = low + 1;
+j = high;
+pivot = a[low];
+while(1) {
+while ((pivot >= a[i]) && i <= high) {
+i++;
+count++;
 }
-void mergesort(int a[10], int left, int right)
-{
- int mid;
- if (left < right)
- {
- mid = (left + right) / 2;
- mergesort(a, left, mid);
- mergesort(a, mid + 1, right);
- merge(a, left, mid, right);
- }
+count++;
+while(pivot < a[j] && j>=low) {
+j--;
+count++;
+}
+count++;
+if(i < j) { }
+else {}
+}
+temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+temp = a[low];
+a[low] = a[j];
+a[j] = temp;
+return j;
+}
+void quicksort(int a[10],int low,int high) {
+int i,j;
+if(low < high) {
+j = partition(a, low, high);
+Dept. of CSE, GAT 20 DAA LAB-CSEL23409
+quicksort(a, low, j-1);
+quicksort(a, j+1, high);
+}
 }
 int main()
 {
- int i, j, n, a[10];
-
- printf("\nEnter no of elements: ");
- scanf("%d", &n);
- printf("\nEnter elements\n");
- for (i = 0; i < n; i++)
- scanf("%d", &a[i]);
- mergesort(a, 0, n - 1);
- printf("\nSorted elements:\n");
- for (i = 0; i < n; i++)
- printf("%d\n", a[i]);
- printf("\nNumber of counts: %d\n", count);
- return 0;
-}
+int a[10];
+int i,n;
+printf("\nEnter no of elements: ");
+scanf("%d",&n);
+printf("\nEnter elements \n");
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+quicksort(a,1,n);
+printf("Sorted elements: \n");
+for(i=1;i<=n;i++)
+printf("%d\t",a[i]);
+printf("\n Number of counts : %d\n",count);
+return 0;
+} 
