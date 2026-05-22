0139de7d5de3024764dadeece4fa1d58bbfa53b5
Gat2428 <albuscharlie44@gmail.com>
2026-05-22 09:04:19 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,61 +1,56 @@
 #include<stdio.h>
+#include<stdlib.h>
 int count;
-int partition(int a[10], int low, int high)
+void merge(int a[10], int left, int mid, int right)
 {
- int i, j, pivot, temp;
- i = low+1;
- j = high;
- pivot = a[low];
- while(1)
- {
- while(pivot>=a[i] && i<=high)
- {
- i++;
- count++;
- }
- count++;
- while(pivot<a[j] && j>=low)
- {
- j--;
- count++;
- }
- count++;
- if(i<j)
- {
- temp = a[i];
- a[i] = a[j];
- a[j] = temp;
- }
- else
- {
- temp = a[low];
- a[low] = a[j];
- a[j] = temp;
- return j;
- }
- }
-}
-void quicksort(int a[10], int low, int high)
+int i, j, k, b[10];
+i = left;
+j = mid+1;
+k = left;
+while( (i<=mid) && (j<=right))
 {
- int i, j;
- if(low<=high)
- {
- j = partition(a, low, high);
- quicksort(a, low, j-1);
- quicksort(a, j+1, high);
- }
+count++;
+if(a[i] < a[j])
+{
 }
+else
+{
+}
+}
+b[k++] = a[i++];
+b[k++] = a[j++];
+while(i <= mid)
+b[k++] = a[i++];
+while(j <= right)
+b[k++] = a[j++];
+for(i=left; i<=right; i++)
+a[i] = b[i];
+}
+void mergesort(int a[10], int left, int right)
+{
+int mid;
+if(left < right)
+{
+mid = (left + right)/2;
+mergesort(a,left,mid);
+mergesort(a,mid+1,right);
+merge(a,left,mid,right);
+}
+}
+Dept. of CSE, GAT 17 DAA LAB-CSEL23409
 int main()
 {
- int i;
- int a[10];
- printf("Enter 10 elements:\n");
- for(int i=1; i<=10; i++)
- scanf("%d", &a[i]);
- quicksort(a, 1, 10);
- printf("Elements after sorting:\n");
- for(int i=1; i<=10; i++)
- printf("%d\t", a[i]);
- printf("\nNumber of counts: %d\n", count);
- return 0;
+int i,j,n,a[10];
+printf("\n Enter no of
+elements: "); scanf("%d",&n);
+printf("\nEnter
+elements\n");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+mergesort(a,0,n-1);
+printf("\n Sorted elements:
+\n"); for(i=0;i<n;i++)
+printf("%d\n",a[i]);
+printf("\n Number of counts :
+%d\n",count); return 0;
 }
