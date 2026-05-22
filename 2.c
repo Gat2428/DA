b0f79c324bbb32bf4f43037788e4b256be17b2ba
Gat2428 <albuscharlie44@gmail.com>
2026-05-22 09:48:36 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,53 +3,48 @@
 int count;
 void merge(int a[10], int left, int mid, int right)
 {
-int i, j, k, b[10];
-i = left;
-j = mid+1;
-k = left;
-while( (i<=mid) && (j<=right))
-{
-count++;
-if(a[i] < a[j])
-{
-}
-else
-{
-}
-}
-b[k++] = a[i++];
-b[k++] = a[j++];
-while(i <= mid)
-b[k++] = a[i++];
-while(j <= right)
-b[k++] = a[j++];
-for(i=left; i<=right; i++)
-a[i] = b[i];
+ int i, j, k, b[10];
+ i = left;
+ j = mid + 1;
+ k = left;
+ while ((i <= mid) && (j <= right))
+ {
+ count++;
+ if (a[i] < a[j])
+ b[k++] = a[i++];
+ else
+ b[k++] = a[j++];
+ }
+ while (i <= mid)
+ b[k++] = a[i++];
+ while (j <= right)
+ b[k++] = a[j++];
+ for (i = left; i <= right; i++)
+ a[i] = b[i];
 }
 void mergesort(int a[10], int left, int right)
 {
-int mid;
-if(left < right)
-{
-mid = (left + right)/2;
-mergesort(a,left,mid);
-mergesort(a,mid+1,right);
-merge(a,left,mid,right);
-}
+ int mid;
+ if (left < right)
+ {
+ mid = (left + right) / 2;
+ mergesort(a, left, mid);
+ mergesort(a, mid + 1, right);
+ merge(a, left, mid, right);
+ }
 }
 int main()
 {
-int i,j,n,a[10];
-printf("\n Enter no of
-elements: "); scanf("%d",&n);
-printf("\nEnter
-elements\n");
-for(i=0;i<n;i++)
-scanf("%d",&a[i]);
-mergesort(a,0,n-1);
-printf("\n Sorted elements:
-\n"); for(i=0;i<n;i++)
-printf("%d\n",a[i]);
-printf("\n Number of counts :
-%d\n",count); return 0;
+ int i, j, n, a[10];
+ printf("\nEnter no of elements: ");
+ scanf("%d", &n);
+ printf("\nEnter elements\n");
+ for (i = 0; i < n; i++)
+ scanf("%d", &a[i]);
+ mergesort(a, 0, n - 1);
+ printf("\nSorted elements:\n");
+ for (i = 0; i < n; i++)
+ printf("%d\n", a[i]);
+ printf("\nNumber of counts: %d\n", count);
+ return 0;
 }
