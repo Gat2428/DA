9c224893f43d5059878f872c10482797740ca748
Gat2428 <albuscharlie44@gmail.com>
2026-07-04 14:18:08 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,40 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 int count;
-int partition(int a[10], int low, int high) {
-int i,j,pivot,temp;
-i = low + 1;
-j = high;
-pivot = a[low];
-while(1) {
-while ((pivot >= a[i]) && i <= high) {
-i++;
-count++;
+int partition(int a[10], int low, int high)
+{
+ int i, j, pivot, temp;
+ i = low+1;
+ j = high;
+ pivot = a[low];
+ while(1)
+ {
+ while(pivot>=a[i] && i<=high)
+ {
+ i++;
+ count++;
+ }
+ count++;
+ while(pivot<a[j] && j>=low)
+ {
+ j--;
+ count++;
+ }
+ count++;
+ if(i<j)
+ {
+ temp = a[i];
+ a[i] = a[j];
+ a[j] = temp;
+ }
+ else
+ {
+ temp = a[low];
+ a[low] = a[j];
+ a[j] = temp;
+ return j;
+ }
+ }
 }
-count++;
-while(pivot < a[j] && j>=low) {
-j--;
-count++;
-}
-count++;
-if(i < j) { }
-else {}
-}
-temp = a[i];
-a[i] = a[j];
-a[j] = temp;
-temp = a[low];
-a[low] = a[j];
-a[j] = temp;
-return j;
-}
-void quicksort(int a[10],int low,int high) {
-int i,j;
-if(low < high) {
-j = partition(a, low, high);
-quicksort(a, low, j-1);
-quicksort(a, j+1, high);
-}
+void quicksort(int a[10], int low, int high)
+{
+ int i, j;
+ if(low<=high)
+ {
+ j = partition(a, low, high);
+ quicksort(a, low, j-1);
+ quicksort(a, j+1, high);
+ }
 }
 int main()
 {
@@ -51,4 +60,4 @@ for(i=1;i<=n;i++)
 printf("%d\t",a[i]);
 printf("\n Number of counts : %d\n",count);
 return 0;
-} 
+}
